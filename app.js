6262d65ae84d2190f7e16aba970accc6916c4e4c
thefutureseer <danielharrislimos@outlook.com>
2023-03-27 17:02:37 -0700

    txt to put a few apps together from my own github

--- app.js
+++ app.js
@@ -21,3 +21,33 @@ const token = jwt.sign(
 )
 
 console.log(token)
+
+const axios = require('axios')
+
+const body = JSON.stringify({
+  external_delivery_id: 'D-12345',
+  pickup_address: '901 Market Street 6th Floor San Francisco, CA 94103',
+  pickup_business_name: 'Wells Fargo SF Downtown',
+  pickup_phone_number: '+16505555555',
+  pickup_instructions: 'Enter gate code 1234 on the callbox.',
+  dropoff_address: '901 Market Street 6th Floor San Francisco, CA 94103',
+  dropoff_business_name: 'Wells Fargo SF Downtown',
+  dropoff_phone_number: '+16505555555',
+  dropoff_instructions: 'Enter gate code 1234 on the callbox.',
+  order_value: 1999,
+})
+
+axios
+  .post('https://openapi.doordash.com/drive/v2/deliveries', body, {
+    headers: {
+      Authorization: 'Bearer ' + token,
+      'Content-Type': 'application/json',
+    },
+  })
+  .then(function (response) {
+    console.log(response.data)
+    console.dir(response.data, {depth: null, colors: true})
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
