e42edf5a8f423a3d559bc72ec7f9d0f53e0a5c28
thefutureseer <danielharrislimos@outlook.com>
2023-04-11 15:17:21 -0700

    add rate post url

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import path from 'path';
-import {fileURLToPath} from 'url'
+import {fileURLToPath} from 'url';
+import 'dotenv/config';
+import {v4 as uuidv4} from 'uuid';
+import { DoorDashClient } from '@doordash/sdk';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -17,3 +21,48 @@ app.listen(PORT, (err) => {
   }
   console.log(`Listening on port: ${PORT}`)
 } );
+
+//Get a delivery rate in the simulation
+app.post("/get-delivery-rate", async (req, res) => {
+
+  //my keys to doordash api
+  const client = new DoorDashClient({
+    developer_id: process.env.developer_id,
+    key_id: process.env.key_id,
+    signing_secret: process.env.signing_secret,
+  });
+
+  //hard coded values from the U.I. to doordash api
+  const response = await client.deliveryQuote({
+    external_delivery_id: uuidv4(),
+    pickup_address: '1227 Folsom street, San Francisco, CA, 94103',
+    pickup_phone_number: '+1(650)7728226',
+    dropoff_address: "Golden Gate park, San Francisco, CA, 94117",
+    dropoff_phone_number: "+1(650)5555555",
+  });
+
+  res.send(response);
+  console.log("Quote", response);
+});
+
+
+//crashing if only creating on postman
+// // accepting
+// app.post("/create-delivery", async (req, res)=>{
+//     //my keys to doordash api
+//     const client = new DoorDashClient({
+//       developer_id: process.env.developer_id,
+//       key_id: process.env.key_id,
+//       signing_secret: process.env.signing_secret,
+//     });
+
+//     //read the values from the delivery id
+//     //session storage will be here
+//     const response = await client.deliveryQuoteAccept(
+//       "0cfdb0e6-a018-4654-ad25-598e272ae71d",
+//     );
+
+//     res.send(response);
+//     //just to see the differnece in terminal
+//     console.log("accept", response );
+// });
--- jwt.js
+++ jwt.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import jwt from 'jsonwebtoken';
 import "dotenv/config";
 
@@ -22,36 +21,37 @@ const token = jwt.sign(
   data,
   Buffer.from(accessKey.signing_secret, 'base64'),
   headers,
-);
+  );
+  
+  console.log(token);
+  
+  
+//   import axios from 'axios';
+  
+//   const body = JSON.stringify({
+//     external_delivery_id: 'D-12345',
+//     pickup_address: '901 Market Street 6th Floor San Francisco, CA 94103',
+//     pickup_business_name: 'Wells Fargo SF Downtown',
+//     pickup_phone_number: '+16505555555',
+//     pickup_instructions: 'Enter gate code 1234 on the callbox.',
+//     dropoff_address: '901 Market Street 6th Floor San Francisco, CA 94103',
+//     dropoff_business_name: 'Wells Fargo SF Downtown',
+//     dropoff_phone_number: '+16505555555',
+//     dropoff_instructions: 'Enter gate code 1234 on the callbox.',
+//     order_value: 1999,
+// })
 
-console.log(token);
-
-
-
-const body = JSON.stringify({
-  external_delivery_id: 'D-12345',
-  pickup_address: '901 Market Street 6th Floor San Francisco, CA 94103',
-  pickup_business_name: 'Wells Fargo SF Downtown',
-  pickup_phone_number: '+16505555555',
-  pickup_instructions: 'Enter gate code 1234 on the callbox.',
-  dropoff_address: '901 Market Street 6th Floor San Francisco, CA 94103',
-  dropoff_business_name: 'Wells Fargo SF Downtown',
-  dropoff_phone_number: '+16505555555',
-  dropoff_instructions: 'Enter gate code 1234 on the callbox.',
-  order_value: 1999,
-})
-
-axios
-  .post('https://openapi.doordash.com/drive/v2/deliveries', body, {
-    headers: {
-      Authorization: 'Bearer ' + token,
-      'Content-Type': 'application/json',
-    },
-  })
-  .then(function (response) {
-    // console.log(response.data)
-    Object.entries(response).forEach(keyValuePair => {console.log("  ",...keyValuePair)})
-  })
-  .catch(function (error) {
-    console.log(error)
-  })
+// axios
+//   .post('https://openapi.doordash.com/drive/v2/deliveries', body, {
+//     headers: {
+//       Authorization: 'Bearer ' + token,
+//       'Content-Type': 'application/json',
+//     },
+//   })
+//   .then(function (response) {
+//     // console.log(response.data)
+//     Object.entries(response).forEach(keyValuePair => {console.log("  ",...keyValuePair)})
+//   })
+//   .catch(function (error) {
+//     console.log(error)
+//   })
